c306758a86fb3e03fcbe1bee72b7a441c727cf52
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-09 17:20:52 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function findMinAndRemoveSorted(arr) {
+    return arr.shift()
+}
 
+function merge(firstArr, secondArr) {
+    const sorted = []
+    while (firstArr.length !==0 && secondArr.length !== 0) {
+        if (firstArr[0] < secondArr[0]) {
+            sorted.push(findMinAndRemoveSorted(firstArr))
+        } else {
+            sorted.push(findMinAndRemoveSorted(secondArr))
+        }
+    }
+    return sorted.concat(firstArr).concat(secondArr)
+}
+
+function mergeSort(array) {
+    let midpoint = array.length/2
+    let first = array.slice(0, midpoint)
+    let second = array.slice(midpoint, array.length)
+    if (array.length < 2) {
+        return array
+    } else {
+        return merge(mergeSort(first), mergeSort(second))
+    }
+}
